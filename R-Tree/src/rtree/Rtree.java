b0e18607eecd8d1166962d6a09d8401fe8c116ea
Wes Clarke <wclar6@live.com>
2018-04-13 18:34:36 +1000

    done

--- R-Tree/src/rtree/Rtree.java
+++ R-Tree/src/rtree/Rtree.java
@@ -2,12 +2,13 @@ package rtree;
 
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
 public final class Rtree {
@@ -15,21 +16,45 @@ public final class Rtree {
 	static ArrayList<Integer> id = new ArrayList<Integer>(); 
 	static ArrayList<Integer> x = new ArrayList<Integer>(); 
 	static ArrayList<Integer> y = new ArrayList<Integer>(); 
+	static ArrayList<int[]> rangeQueries = new ArrayList<int[]>();
+	static ArrayList<Integer> results = new ArrayList<Integer>();
 	private static int M;
-	private int L;
 	private static Node root;
 	private static int result;
 	
 	public static void main(String[] args) {
-		loadDataset("C:/Users/wclar/Desktop/dataset.txt");
-		Rtree r = new Rtree(4);
-		for (int i =0; i<16; i++){
-			System.out.println(i +  " / "  + id.size());
+		if (args.length != 4) {
+			System.out.println("Usage: datasetPath queryPath savePath m");
+		}
+		String datasetLocation = args[0];
+		String queryLocation = args[1];
+		String saveLocation = args[2];
+		//Max number of points per node
+		int m = Integer.parseInt(args[3]);
+		
+		loadDataset(datasetLocation);
+		loadQueries(queryLocation);
+		Rtree r = new Rtree(m);
+		ArrayList<Integer> storeResults = new ArrayList<Integer>();
+		long startTime = System.nanoTime();
+		for (int i =0; i<id.size(); i++){
 			r.insert(root, id.get(i));
 		}
-		int[] range = {57, 0, 65, 100000};
-		Rtree.range_query(root, range);
-		System.out.println("Root MBR: " +"("+root.MBR[0] + "," + root.MBR[1]+ ")" + "("+root.MBR[2] + "," + root.MBR[3]+ ")");
+		long endTime = System.nanoTime();
+		long duration = (endTime - startTime);
+		System.out.println("Runtime to build tree " + duration + " Nanoseconds");
+		
+		long startTime1 = System.nanoTime();
+		for (int i=0; i<rangeQueries.size(); i++) {
+			int[] range = rangeQueries.get(i);
+			int res = Rtree.range_query(root, range);
+			storeResults.add(res);
+			result = 0;
+		}
+		long endTime1 = System.nanoTime();
+		long duration1 = (endTime1 - startTime1);
+		System.out.println("Runtime for range query is " + duration1 + " Nanoseconds");
+		saveResults(saveLocation, storeResults, duration1);
 	}
 	
 	/**
@@ -39,8 +64,7 @@ public final class Rtree {
 	 */
 	public Rtree(int B) {
 		M = B;
-		this.L = (int) Math.ceil(0.4 * M);
-		this.root = createRoot(true);
+		Rtree.root = createRoot(true);
 	}
 
 	
@@ -57,49 +81,44 @@ public final class Rtree {
 	public void insert(Node u, int id){
 		Entry e = new Entry(id);
 		if (u.leaf) {
-			System.out.println("Inserting at leaf");
+			//System.out.println("Inserting at leaf");
 			u.addLeafNode(e);
 			e.parent = u;
-			System.out.println("Root MBR: " +"("+root.MBR[0] + "," + root.MBR[1]+ ")" + "("+root.MBR[2] + "," + root.MBR[3]+ ")");
+			//System.out.println("Root MBR: " +"("+root.MBR[0] + "," + root.MBR[1]+ ")" + "("+root.MBR[2] + "," + root.MBR[3]+ ")");
 			if (u.leafNode.size() > M) {
 				handle_overflow(u);
 			}
 		} else {
-			System.out.println("Looking for Leaf");
+			//System.out.println("Choosing SUb tree");
 			Node v = chooseSubTree(u, id);
 			insert(v, id);
 		}
 	}
 	
-	private static void range_query(Node u, int [] range) {
+	private static int range_query(Node u, int [] range) {
 		if (u.leaf){
-			System.out.println("Range Searching Leaf");
+			//System.out.println("Range Searching Leaf");
 			for (int i = 0; i < u.leafNode.size(); i++) {
 				int id = u.leafNode.get(i).id;
-				System.out.println("Leaf Visited: "+id);
+				//System.out.println("Leaf Visited: "+id);
 				int [] point = {x.get(id-1), y.get(id-1)};
 				if (isCovered(range, point)){
-					System.out.println("ID: " + id);
-					System.out.println("Coord " + point[0] + " , " + point[1]);
+					//System.out.println("ID: " + id);
 					result++;
-					System.out.println("Excitement");
-					System.out.println(result);
 				}
 				
 			}
 		} else {
-			System.out.println("Range Searching Internal");
-			for (Node n : u.children) {
-				System.out.println("Child Node has MBR: " +"("+n.MBR[0] + "," + n.MBR[1]+ ")" + "("+n.MBR[2] + "," + n.MBR[3]+ ")");
-			}
+			//System.out.println("Range Searching Internal");
 			for (Node n : u.children){
 				if (intersects(range, n.MBR)){
 					range_query(n,range);
 				}
 			}
 		}
+		return result;
 	}
-	
+	//Checks if a point is inside the rectange range
 	private static boolean isCovered(int [] range, int [] point) {
 		if (point[0] >= range[0] && point[0] <= range[2] && point[1] >= range[1] && point[1] <= range[3]){
 			return true;
@@ -107,7 +126,7 @@ public final class Rtree {
 			return false;
 		}
 	}
-	
+	//Checks if an MBR intersects the rectangle range
 	private static boolean intersects(int[] range, int[] MBR){
 		//rect[0] = lx, rect[1] = ly, rect[2] = ux, rect[3] = uy
 		int mlx = MBR[0];
@@ -139,11 +158,12 @@ public final class Rtree {
 		boolean leaf;
 		Node u;
 		Node udash;
-		System.out.println("Handle Overflow");
+		//System.out.println("Handle Overflow");
 		ArrayList<Node> split = new ArrayList<Node>();
 		leaf = in.leaf;
 		split = splitNode(in);
 		if (leaf){
+			//System.out.println("Overflow of leaf");
 			u = new Node(true);
 			for (Entry e : split.get(0).leafNode){
 				u.addLeafNode(e);
@@ -153,6 +173,7 @@ public final class Rtree {
 				udash.addLeafNode(e);
 			}
 		} else {
+			//System.out.println("Overflow of Internal");
 			u = new Node(false);
 			for (Node e : split.get(0).children){
 				u.addNode(e);
@@ -163,64 +184,81 @@ public final class Rtree {
 			}
 		}
 		if (in == root) {
-			System.out.println("Overflow at root");
+			//System.out.println("Overflow at root");
 			root = createRoot(false);
 			root.addNode(u);
 			root.addNode(udash);
 		} else {
-			System.out.println("Overflow elsewhere");
+			//System.out.println("Overflow elsewhere");
 			Node w = new Node(false);
 			u.parent = w;
 			udash.parent = w;
 			w.addNode(u);
 			w.addNode(udash);
-			if (w.children.size() > M){
-				System.out.println("Created new overflow");
-				handle_overflow(w);
+			in.parent.addNode(w);
+			if (in.parent.children.size() > M){
+				//System.out.println("Created new overflow");
+				handle_overflow(in.parent);
 			}
 		}
 	}
 
 
 	private static ArrayList<Node> splitNode(Node in) {
-		
-
 		ArrayList<int[]> s1 = new ArrayList<int[]>();
 		ArrayList<int[]> s2 = new ArrayList<int[]>();
 		int bestPerim = 90000000;
-		
 		ArrayList<Node> bestSplit = new ArrayList<Node>();
+		//Allows us to sort points by x or y
+		class sortPoints implements Comparator<int []> {
+	    	private int compareBy = 0;
+	    	 public sortPoints(int compareBy) {
+	    	        this.compareBy = compareBy;
+	    	    }
+
+	        public int compare(int [] a, int [] b) {
+	        	if (compareBy == 0){
+	        		return a[0] - b[0];
+	        	} else if (compareBy == 0){
+	        		return a[1] - b[1];
+	        	} else
+	        	return a[0] - b[0];
+	        }
+	    }
+		//Allows us to sort rectangles lx or ux or ly or uy
+	    class sortRectangles implements Comparator<int []> {
+	    	private int compareBy = 0;
+	    	 public sortRectangles(int compareBy) {
+	    	        this.compareBy = compareBy;
+	    	    }
+
+	        public int compare(int [] a, int [] b) {
+	        	if (compareBy == 0){
+	        		return a[0] - b[0];
+	        	} else if (compareBy == 0){
+	        		return a[2] - b[2];
+	        	} else if (compareBy == 1){
+	        		return a[1] - b[1];
+	        	} else if (compareBy == 2) {
+	        		return a[3] - b[3];
+	        	}
+	        	return a[0] - b[0];
+	        }
+	    }
+	
 		if (in.leaf == false) {
-			System.out.println("Spliting internal Node");
-			ArrayList<List<int []>> splitNodes = new ArrayList<List<int []>>();
+			//System.out.println("Spliting internal Node");
+			ArrayList<int[]> splitNodes = new ArrayList<int[]>();
 			int m = in.children.size();
 			ArrayList<int[]> recList = new ArrayList<int[]>();
 		for (int i =0; i<m;i++){
 			int[] rec = {in.children.get(i).MBR[0], in.children.get(i).MBR[1], in.children.get(i).MBR[2],in.children.get(i).MBR[3], i};
 			recList.add(rec);
 		}
+
 		for (int k =0; k<4;k++) {
-
-		    class sortl implements Comparator<int []> {
-		    	private int compareBy = 0;
-		    	 public sortl(int compareBy) {
-		    	        this.compareBy = compareBy;
-		    	    }
-
-		        public int compare(int [] a, int [] b) {
-		        	if (compareBy == 0){
-		        		return a[0] - b[0];
-		        	} else if (compareBy == 0){
-		        		return a[2] - b[2];
-		        	} else if (compareBy == 1){
-		        		return a[1] - b[1];
-		        	} else if (compareBy == 2) {
-		        		return a[3] - b[3];
-		        	}
-		        	return a[0] - b[0];
-		        }
-		    }
-			Collections.sort(recList, new sortl(k));
+			
+			Collections.sort(recList, new sortRectangles(k));
 			for (int i=(int) Math.ceil(0.4*M); i<=m-Math.ceil(0.4*M); i++){
 				for (int j = 0; j<i; j++) {
 					s1.add(recList.get(j));
@@ -231,32 +269,43 @@ public final class Rtree {
 				int perims1 = perimSum(getMBR(s1));
 				int perims2 = perimSum(getMBR(s2));
 				int totalPerim = perims1 + perims2;
-				s1.clear();
-				s2.clear();
 				if (totalPerim < bestPerim){
 					splitNodes.clear();
 					bestPerim = totalPerim;
-					List<int[]> u= recList.subList(0, i);
-					List<int[]> udash= recList.subList(i, recList.size());
+					int[] u = new int[s1.size()];
+					int[] udash = new int[s2.size()];
+					int l=0;
+					for(int n =0; n<i;n++){
+						u[l] = n;
+						l++;
+					}
+					l=0;
+					for(int n =i; n<recList.size();n++){
+						udash[l] = n;
+						l++;
+					}
 					splitNodes.add(u);
 					splitNodes.add(udash);
 				}
+				s1.clear();
+				s2.clear();
 			}
 		}
-		Node u = new Node(false);
-		Node udash = new Node(false);
-		for (int i =0; i<splitNodes.get(0).size(); i++) {
-			u.addNode(in.children.remove(splitNodes.get(0).get(i)[4]));
+		Node u = new Node(true);
+		Node udash = new Node(true);
+		for (int i =0; i<splitNodes.get(0).length; i++) {
+			u.addNode(in.children.get(splitNodes.get(0)[i]));
 		}
-		for (int i =0; i<splitNodes.get(1).size(); i++) {
-			udash.addNode(in.children.remove(splitNodes.get(1).get(i)[4]));
+		for (int i =0; i<splitNodes.get(1).length; i++) {
+			udash.addNode(in.children.get(splitNodes.get(1)[i]));
 		}
+		in.children.clear();
 		bestSplit.add(u);
 		bestSplit.add(udash);
 		return bestSplit;
 		} else {
 			ArrayList<int[]> splitNodes = new ArrayList<int[]>();
-			System.out.println("Splitting Leaf");
+			//System.out.println("Splitting Leaf");
 			int m = in.leafNode.size();
 			ArrayList<int[]> coord = new ArrayList<int []>();
 			for (int i =0; i<m;i++){
@@ -264,26 +313,9 @@ public final class Rtree {
 				int [] c = {x.get(id-1), y.get(id-1), id};
 				coord.add(c);
 			}
-			/*for (int i=0; i<coord.size();i++){
-				System.out.println(coord.get(i)[0] + " , " +coord.get(i)[1]);
-			}*/
+			
 			for (int k = 0; k<2;k++){
-				class sorta implements Comparator<int []> {
-			    	private int compareBy = 0;
-			    	 public sorta(int compareBy) {
-			    	        this.compareBy = compareBy;
-			    	    }
-
-			        public int compare(int [] a, int [] b) {
-			        	if (compareBy == 0){
-			        		return a[0] - b[0];
-			        	} else if (compareBy == 0){
-			        		return a[1] - b[1];
-			        	} else
-			        	return a[0] - b[0];
-			        }
-			    }
-				Collections.sort(coord, new sorta(k));	
+				Collections.sort(coord, new sortPoints(k));	
 				for (int i=(int) Math.ceil(0.4*M); i<=(int)(m-Math.ceil(0.4*M)); i++){
 					
 					for (int j = 0; j<i; j++) {
@@ -335,6 +367,7 @@ public final class Rtree {
 
 	
 	private Node chooseSubTree(Node u, int id) {
+		//System.out.print("Is Leaf: " + u.leaf + "\n");
 		if (u.leaf) {
 			return u;
 		}
@@ -342,13 +375,20 @@ public final class Rtree {
 		int minInc = 9000000;
 		for (Node c: u.children) {
 			int inc = perimeterInc(c, id);
+			//System.out.println("Min Increase :" + minInc);
+			//System.out.println("New Increase :" + inc);
 			if (inc < minInc){
 				minInc = inc;
 				next = c;
 			} else if (inc == minInc){
+				//System.out.println("TIE BREAKER!");
 				int nextPerim = perimSum(next.MBR);
 				int cPerim = perimSum(c.MBR);
+				//System.out.println("C PERIM :" + cPerim);
+				//System.out.println("nextPerim :" + nextPerim);
 				if (cPerim < nextPerim){
+					next = c;
+				} else {
 					next = c;
 				}
 			}
@@ -376,7 +416,6 @@ public final class Rtree {
 		}
 		int newPerim = 2*(ux-lx) + 2*(uy-ly);
 		return newPerim-perim;
-		
 	}
 	
 	//Return Perimeter Sum of the MBR
@@ -431,10 +470,43 @@ public final class Rtree {
 		}
 		long endTime = System.nanoTime();
 		//Run time in miliseconds
-		long duration = (endTime - startTime)/1000000;
-		System.out.println("Runtime is " + duration + " Milliseconds");
+		long duration = (endTime - startTime);
+		System.out.println("Runtime to scan file " + duration + " Nanoseconds");
+	}
+	
+	public static void loadQueries(String filePath) {
+		try {
+		int lx;
+		int ly;
+		int ux;
+		int uy;
+		File file = new File(filePath);
+		Scanner scanner = new Scanner(file);
+		while(scanner.hasNextInt()) {
+			lx = (scanner.nextInt());
+			ux = (scanner.nextInt());
+			ly = (scanner.nextInt());
+			uy = (scanner.nextInt());
+			int[] query = {lx, ly, ux, uy}; 
+			rangeQueries.add(query);
+		}
+		scanner.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	public static void saveResults(String filePath, ArrayList<Integer> results, long duration){
+		try (PrintWriter out = new PrintWriter(filePath)) {
+			for (int i=0; i<results.size();i++){
+		    out.println(results.get(i));
+			}
+			out.println("Total Runtime: " + duration + " Nanoseconds");
+			out.println("Average Runtime: " + duration/rangeQueries.size() + " Nanoseconds");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 	 
 	
 	private static class Node {
@@ -457,6 +529,7 @@ public final class Rtree {
 	        	ArrayList<Integer> xlist = new ArrayList<Integer>();
 	        	ArrayList<Integer> ylist = new ArrayList<Integer>();
 	        	this.leafNode.add(e);
+	        	e.parent = this;
 	        	for (Entry i : this.leafNode) {
 	        		int id = i.id;
 	        		xlist.add(x.get(id-1));
@@ -471,6 +544,7 @@ public final class Rtree {
 	        	ArrayList<Integer> xlist = new ArrayList<Integer>();
 	        	ArrayList<Integer> ylist = new ArrayList<Integer>();
 	        	this.children.add(n);
+	        	n.parent = this;
 	        	for (Node e : this.children) {
 	        		xlist.add(e.MBR[0]);
 	        		xlist.add(e.MBR[2]);
